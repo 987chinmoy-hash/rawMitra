900a4f7831588d44ad4bbaa6bb711ab047de8a51
Chinmoy Hazarika <987chinmoy@gmail.com>
2026-09-06 04:49:48 +0530

    Update ArtisanOrderConfirm.jsx

--- src/pages/artisan/ArtisanOrderConfirm.jsx
+++ src/pages/artisan/ArtisanOrderConfirm.jsx
@@ -83,8 +83,7 @@ export default function ArtisanOrderConfirm() {
               pricePerUnit: order.pricePerUnit,
               transportCharge: order.transportTotal || order.transportCharge,
               validity: order.validity,
-              
-              neededBy: order.neededBy,
+        
               
               perArtisan: (order.perArtisan || []).map((p) => ({
                 artisanId: p.artisanId,
